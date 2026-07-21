2bd78896af243ea0e3cb2c2ffbe7a1b81012377f
Mr X <93528905+JulesZYTB@users.noreply.github.com>
2026-07-21 03:40:19 +0200

    Updated next.config.js formatting

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,25 +1,28 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 let appConfig = {};
 try {
-  appConfig = JSON.parse(fs.readFileSync(path.join(__dirname, '../config.json'), 'utf8'));
-} catch (e) {
-  console.log('No config.json found or invalid format.');
+  appConfig = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "../config.json"), "utf8"),
+  );
+} catch {
+  console.log("No config.json found or invalid format.");
 }
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  output: 'export',
-  distDir: process.env.NODE_ENV === 'production' ? '../app' : '.next',
+  output: "export",
+  distDir: process.env.NODE_ENV === "production" ? "../app" : ".next",
   trailingSlash: true,
   env: {
-    NEXT_PUBLIC_SITE_URL: appConfig.NEXT_PUBLIC_SITE_URL || process.env.NEXT_PUBLIC_SITE_URL,
+    NEXT_PUBLIC_SITE_URL:
+      appConfig.NEXT_PUBLIC_SITE_URL || process.env.NEXT_PUBLIC_SITE_URL,
   },
   images: {
     unoptimized: true,
   },
   webpack: (config) => {
-    return config
+    return config;
   },
-}
+};
