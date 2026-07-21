552cb7365800b78408dfabf503a364ef8f2d8d58
Mr X <93528905+JulesZYTB@users.noreply.github.com>
2026-07-21 03:40:05 +0200

    Added locales exports

--- main/services/index.ts
+++ main/services/index.ts
@@ -1,9 +1,28 @@
-export { startLocalServer, PROD_PORT } from './server'
-export { handleDeepLink } from './deeplink'
-export { initPermissions } from './permissions'
-export { initTray, buildTrayMenu, getAppLocale, getI18n } from './tray'
-export { initUpdater } from './updater'
-export { prewarmBadgeCache, initBadge, createBadgeIcon, createMicOverlayIcon, handleSetBadgeCount, handleSetVoiceActive, getCurrentBadgeCount } from './badge'
-export { registerIpcHandlers } from './ipc-handlers'
-export { startRpcPolling, stopRpcPolling } from './rpc'
-export { handleThumbarVoiceActive, handleSetMuteState, reapplyThumbar } from './thumbar'
+export { startLocalServer, PROD_PORT } from "./server";
+export { handleDeepLink } from "./deeplink";
+export { initPermissions } from "./permissions";
+export { initTray, buildTrayMenu } from "./tray";
+export {
+  getAppLocale,
+  getI18n,
+  getExternalLinkI18n,
+  resolveMainProcessLocale,
+} from "./locales";
+export { confirmAndOpenExternal } from "./external-link";
+export { initUpdater } from "./updater";
+export {
+  prewarmBadgeCache,
+  initBadge,
+  createBadgeIcon,
+  createMicOverlayIcon,
+  handleSetBadgeCount,
+  handleSetVoiceActive,
+  getCurrentBadgeCount,
+} from "./badge";
+export { registerIpcHandlers } from "./ipc-handlers";
+export { startRpcPolling, stopRpcPolling } from "./rpc";
+export {
+  handleThumbarVoiceActive,
+  handleSetMuteState,
+  reapplyThumbar,
+} from "./thumbar";
